d4dd8a3a6cbc3430e6e9cf6f25c586d66b0dac6c
Jeff Luszcz <jeff@luszcz.com>
2023-06-07 09:31:05 -0700

    Update PurpleTheopolis.ino copyright date to 2023

--- PurpleTheopolis.ino
+++ PurpleTheopolis.ino
@@ -7,7 +7,7 @@
 #include "arduino_secrets.h"
 
 
-/* Copyright 2022 Jeff Luszcz
+/* Copyright 2023 Jeff Luszcz
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the 
 Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
